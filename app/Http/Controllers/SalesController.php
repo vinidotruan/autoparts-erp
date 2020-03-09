1e1320ea12405fb0c852b5f65a9c4863a7b57194
Ruan Vinícius <contatoruanvinicius@gmail.com>
2020-03-09 15:33:55 -0300

    - Add SalesController  - Add sales resources functions

--- app/Http/Controllers/SalesController.php
+++ app/Http/Controllers/SalesController.php
@@ -14,7 +14,9 @@ class SalesController extends Controller
      */
     public function index()
     {
-        //
+        $sales = Sales::paginate(15);
+
+        return response()->json(['sales' => $sales]);
     }
 
     /**
@@ -25,40 +27,46 @@ class SalesController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $sale = Sales::create($request->all());
+
+        return response()->json(['message' => 'Sale created']);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Sales  $sales
+     * @param  \App\Sales  $sale
      * @return \Illuminate\Http\Response
      */
-    public function show(Sales $sales)
+    public function show(Sales $sale)
     {
-        //
+        $response = Sales::find($sale->id);
+        return response()->json(['sale' => $response]);
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Sales  $sales
+     * @param  \App\Sales  $sale
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Sales $sales)
+    public function update(Request $request, Sales $sale)
     {
-        //
+        $sale->update($request->all());
+        return response()->json(['message' => "Sale updated"]);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Sales  $sales
+     * @param  \App\Sales  $sale
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Sales $sales)
+    public function destroy(Sales $sale)
     {
-        //
+        $sale->delete();
+
+        return response()->json(["message" => "Sale deleted"]);
     }
 }
