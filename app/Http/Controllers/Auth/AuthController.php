56092d4ce09dcc0ae06384ccfda5a4986c448aa7
Ruan Vinícius <contatoruanvinicius@gmail.com>
2020-03-12 11:41:47 -0300

    - Add id to logged user return

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -82,6 +82,7 @@ class AuthController extends Controller
         return response()->json([
             'access_token' => $tokenResult->accessToken,
             'token_type' => 'Bearer',
+            'id' => $user->id,
             'expires_at' => Carbon::parse(
                 $tokenResult->token->expires_at
             )->toDateTimeString()
