9f9e28d1e19d11ba64c896844ebbec252c0c1acc
Ruan Vinícius <contatoruanvinicius@gmail.com>
2020-03-25 16:42:30 -0300

    - Add translate errors message  - Add validate on store product controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -26,7 +26,17 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
+        $validatedData = $request->validate([
+            'title' => 'required|unique:products',
+            'ref' => 'required|unique:products',
+            'value_cost' => 'required',
+            'value_sell' => 'required',
+            'amount' => 'required',
+            'category_id' => 'required',
+        ]);
+
         $product = Product::create($request->all());
+        
         return response()->json(['product' => $product, 'message' => 'Product created']);
     }
 
